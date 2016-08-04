1320597563ca4e2e831d94c694ac385183f66476
pu3549zi4 <xmart@xmartdemac-mini.local>
2016-08-04 17:07:45 +0800

    Initial Commit

---
+++ StarStackView-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarStackView-OC
+//
+//  Created by Xmart on 16/8/4.
+//  Copyright © 2016年 Paul.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarStackView-OC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StarStackView-OC
+//
+//  Created by Xmart on 16/8/4.
+//  Copyright © 2016年 Paul.Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
